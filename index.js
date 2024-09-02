28996765e06c83bd9da63e589bc3858c1f175bf2
Galaktikshapes <tmakgakga@gmail.com>
2024-09-02 23:14:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+const originalUrls = [];
+const shortUrls = [];
+
+// Your first API endpoint
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url;
+  const foundIndex = originalUrls.indexOf(url);
+
+  if (!url.includes("https://") && !url.includes("http://")) {
+    return res.json({ error: 'invalid url' })
+  }
+
+  if (foundIndex < 0) {
+    originalUrls.push(url);
+    shortUrls.push(shortUrls.length);
+
+    return res.json({
+      original_url: url,
+      short_url: shortUrls.length -1
+    })
+  }
+
+  res.json({ 
+    original_url: url,
+    short_url: shortUrls[foundIndex]
+   });
+});
+
+app.get("/api/shorturl/:shorturl", (req, res) => {
+  const shorturl = parseInt(req.params.shorturl);
+  const foundIndex = shortUrls.indexOf(shorturl);
+
+  if (foundIndex < 0) {
+    res.json({
+      error: "No short URL found for the given input"
+    })
+  }
+  res.redirect(originalUrls[foundIndex])
+})
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
